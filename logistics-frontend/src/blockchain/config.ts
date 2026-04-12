7f98a09b3568c95232e2a0ece004e68632583edc
Luis Carlos Gracia Puentes <123645497+luiscgracia@users.noreply.github.com>
2026-04-12 09:29:34 -0500

    ok

--- logistics-frontend/src/blockchain/config.ts
+++ logistics-frontend/src/blockchain/config.ts
@@ -3,9 +3,10 @@ import { anvil } from 'wagmi/chains'
 import { injected } from 'wagmi/connectors'
 import LogisticsArtifact from '../../../out/LogisticsTracking.sol/LogisticsTracking.json'
 
-// Leemos la dirección desde las variables de entorno de Vite
-export const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3" as `0x${string}`;
-// export const CONTRACT_ADDRESS = import.meta.env.VITE_CONTRACT_ADDRESS as '0x${string}';
+// Leemos la dirección del contrato desde las variables de entorno de Vite
+import deployment from '../../../broadcast/DeployLogistics.s.sol/31337/run-latest.json'
+export const CONTRACT_ADDRESS = deployment.transactions[0].contractAddress as `0x${string}`;
+// export const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3" as `0x${string}`;
 
 export const ACTOR_ROLES = ['NONE', 'SENDER', 'CARRIER', 'HUB', 'RECIPIENT', 'INSPECTOR'];
 
